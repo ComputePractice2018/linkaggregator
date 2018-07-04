15e6e4d4b1c4f430a520f239d4d4674e37aea479
Anastasia Borovaya <normanb624@gmail.com>
2018-07-04 18:40:50 +0300

    Убрано все лишнее из проекта Задача #2

---
+++ frontend/src/components/LinkAggregator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="linkaggregator">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LinkAggregator',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
