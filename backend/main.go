746cf3d07972e873e88ca8da3e700c130729603a
cobaku <txdm@ya.ru>
2018-07-09 18:58:11 +0300

    Функция вынесена в отдельный пакет Issue #10

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/ComputePractice2018/linkaggregator/backend/utils"
 )
 
-//Возвращение произведение двух чисел
-func MultiplyTwoInt(first, second int) int {
-	return first * second
-}
 func main() {
 	fmt.Print("Hello world \n")
-	fmt.Printf("Sum of two int: %d\n", MultiplyTwoInt(3, 16))
+	fmt.Printf("Sum of two int: %d\n", utils.MultiplyTwoInt(3, 16))
 }
