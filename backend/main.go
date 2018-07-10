07a5f50d9913a576a4fab5a00c0bd2869444bb21
cobaku <txdm@ya.ru>
2018-07-10 23:24:35 +0300

    Сделано рабочее RestApi
    issue #26

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/ComputePractice2018/linkaggregator/backend/web"
 	"net/http"
 	"log"
 )
 
 func main() {
-	http.HandleFunc("/api/subscription/list", web.GetSubscriptions)
-	http.HandleFunc("/api/subscription/add", web.AddSubscription)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter()
+	router.HandleFunc("/api/subscriptions", web.GetSubscriptions).Methods("GET")
+	router.HandleFunc("/api/subscriptions", web.AddSubscription).Methods("POST")
+	router.HandleFunc("/api/subscriptions/{id}", web.EditSubscription).Methods("PUT")
+	router.HandleFunc("/api/subscriptions/{id}", web.DeleteSubscription).Methods("DELETE")
+
+	router.HandleFunc("/api/feed", web.GetFeed).Methods("GET")
+	router.HandleFunc("/api/feed/{id}", web.GetFeedById).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
